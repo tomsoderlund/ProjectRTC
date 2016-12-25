42f7504db64a4e558f0043d3b36d3e3a59526af0
Tom Söderlund <tom@tomsoderlund.com>
2016-12-25 09:47:48 +0100

    Allow CORS

--- app/routes.js
+++ app/routes.js
@@ -12,6 +12,20 @@ module.exports = function(app, streams) {
                         });
   };
 
+  // Allow CORS headers: http://stackoverflow.com/questions/18310394/no-access-control-allow-origin-node-apache-port-issue
+  var allowCORS = function (req, res, next) {
+      // Website you wish to allow to connect
+      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000');
+      // Request methods you wish to allow
+      res.setHeader('Access-Control-Allow-Methods', 'GET'); // , POST, OPTIONS, PUT, PATCH, DELETE
+      // Request headers you wish to allow
+      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+      // Set to true if you need the website to include cookies in the requests sent to the API (e.g. in case you use sessions)
+      res.setHeader('Access-Control-Allow-Credentials', true);
+      // Pass to next layer of middleware
+      next();
+  };
+
   // GET streams as JSON
   var displayStreams = function(req, res) {
     var streamList = streams.getStreams();
@@ -21,7 +35,7 @@ module.exports = function(app, streams) {
     res.status(200).json(data);
   };
 
-  app.get('/streams.json', displayStreams);
+  app.get('/streams.json', allowCORS, displayStreams);
   app.get('/', index);
   app.get('/:id', index);
 }
